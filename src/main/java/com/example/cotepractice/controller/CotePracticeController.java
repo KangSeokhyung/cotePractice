b2c86aac5e7d271e462da790ae123eeb5899ea92
강석형 <tjrgudtm@naver.com>
2024-12-03 22:43:59 +0900

    코테 3문제 추가

--- src/main/java/com/example/cotepractice/controller/CotePracticeController.java
+++ src/main/java/com/example/cotepractice/controller/CotePracticeController.java
@@ -1,6 +1,7 @@
 package com.example.cotepractice.controller;
 
 import com.example.cotepractice.service.CoteService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class CotePracticeController {
     private final CoteService coteService;
 
     @GetMapping("/problem1")
+    @Operation(summary = "프로그래머스 문자열 출력하기", description = "문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.")
     public String problem1(
             @Parameter(description = "입력받을 문자열") @RequestParam String inStr) {
 
@@ -23,9 +25,28 @@ public class CotePracticeController {
     }
 
     @GetMapping("/problem2")
+    @Operation
     public String problem2(
             @Parameter(description = "입력받을 문자열1") @RequestParam String inStr,
             @Parameter(description = "입력받을 문자열2") @RequestParam String inStr2){
         return coteService.problem2(inStr, inStr2);
+    }
+
+    @GetMapping("/problem3")
+    public String problem3(
+            @Parameter(description = "입력받을 문자열1") @RequestParam String inStr,
+            @Parameter(description = "입력받을 문자열2") @RequestParam int cnt){
+        return coteService.problem3(inStr, cnt);
+    }
+
+    @GetMapping("/problem4")
+    public String problem4(
+            @Parameter(description = "입력받을 문자열1") @RequestParam String inStr){
+        return coteService.problem4(inStr);
+    }
+
+    @GetMapping("/problem5")
+    public String problem5(){
+        return coteService.problem5();
     }
 }
--- src/main/java/com/example/cotepractice/service/CoteService.java
+++ src/main/java/com/example/cotepractice/service/CoteService.java
@@ -25,4 +25,44 @@ public class CoteService {
         String s = "a = " + inputStr1 + "\n" + "b = " + inputStr2;
         return s;
     }
+
+    // 문제3. 문자열 반복
+    public String problem3(String inputStr1, int cnt) {
+        String rtn_string = "";
+        if (!inputStr1.isEmpty() && inputStr1.length() <= 10) {
+            System.out.print(inputStr1);
+        }
+        for(int i = 0; i < cnt; i++) {
+            rtn_string += inputStr1;
+        }
+        return rtn_string;
+    }
+
+    // 문제4. 문자열 대/소문자 변환
+    public String problem4(String inputStr1) {
+        String rtn_string = "";
+        String temp_String = "";
+        if (!inputStr1.isEmpty() && inputStr1.length() <= 20) {
+            System.out.print(inputStr1);
+        }
+
+       for(char c : inputStr1.toCharArray()) {
+           if (Character.isUpperCase(c)) {
+               temp_String = String.valueOf(c);
+               rtn_string += temp_String.toLowerCase();
+           }else if(Character.isLowerCase(c)){
+               temp_String = String.valueOf(c);
+               rtn_string += temp_String.toUpperCase();
+           }
+       }
+
+        return rtn_string;
+    }
+
+    // 문제5. 특수문자 출력
+    public String problem5() {
+        String rtn_string = "!@#$%^&*(\\'\"<>?:;";
+
+        return rtn_string;
+    }
 }
